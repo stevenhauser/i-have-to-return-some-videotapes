a01f534cece74f74532bdbbd6b2741650e64af1b
Steven Hauser <stevenhauser@gmail.com>
2015-11-06 13:56:36 -0500

    Rework Entities and Entity into Tiles and Tile

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -6,25 +6,27 @@ import classNames from 'classnames';
 import { createPureComponent } from 'utils/createPureComponent';
 import { gridCoordsToStyle } from 'utils/gridCoordsToStyle';
 
-import 'components/Entity/Entity.scss';
+import 'components/Tile/Tile.scss';
 
 export default createPureComponent({
 
-  displayName: 'Entity',
+  displayName: 'Tile',
 
   propTypes: {
+    block: PropTypes.string.isRequired,
     col: PropTypes.number.isRequired,
     row: PropTypes.number.isRequired,
     type: PropTypes.string.isRequired,
   },
 
   render() {
-    const { col, row, type } = this.props;
+    const { block, col, row, type } = this.props;
     const attrs = {
       style: gridCoordsToStyle(row, col),
       className: classNames(
-        'entity',
-        `entity--${type}`
+        'tile',
+        block,
+        `${block}--${type}`
       )
     };
     return (<div {...attrs} />);
--- src/components/Tiles/Tiles.js
+++ src/components/Tiles/Tiles.js
@@ -3,43 +3,45 @@ import ReactDOM from 'react-dom';
 
 import { createPureComponent } from 'utils/createPureComponent';
 
-import Entity from 'components/Entity/Entity';
+import Tile from 'components/Tile/Tile';
 
 export default createPureComponent({
 
-  displayName: 'Entities',
+  displayName: 'Tiles',
 
   propTypes: {
+    block: PropTypes.string.isRequired,
     // TODO: This has problems with `null` values. Fix it.
-    // entities: PropTypes.arrayOf(
-    //   PropTypes.shape(Entity.propTypes)
+    // tiles: PropTypes.arrayOf(
+    //   PropTypes.shape(Tile.propTypes)
     // )
   },
 
-  renderEntity(row, col, entity) {
-    return !entity ? null : (
-      <Entity
-        key={`${entity.type}-${row}-${col}`}
+  renderTile(block, row, col, tile) {
+    return !tile ? null : (
+      <Tile
+        key={`${tile.type}-${row}-${col}`}
+        block={block}
         col={col}
         row={row}
-        {...entity}
+        type={tile.type}
       />
     );
   },
 
   render() {
-    const { entities } = this.props;
-    const nodes = entities.reduce((arr, row, ri) => (
+    const { block, tiles } = this.props;
+    const nodes = tiles.reduce((arr, row, ri) => (
       [
         ...arr,
         // Note that we don't `filter` here because we need
         // to preserve the row and column indicies. `filter`ing
         // would mess up the columns and therefore the level grid.
-        ...row.map((e, ci) => this.renderEntity(ri, ci, e))
+        ...row.map((tile, ci) => this.renderTile(block, ri, ci, tile))
       ]
     ), []);
     return (
-      <div className="entities">
+      <div className="tiles">
         {nodes}
       </div>
     );
