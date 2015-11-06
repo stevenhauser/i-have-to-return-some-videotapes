473ee30901ccb7c5b5053ac41adabf47cef11e24
Steven Hauser <stevenhauser@gmail.com>
2015-11-06 17:01:33 -0500

    Don't let player go out of bounds

--- src/state/actions/move.js
+++ src/state/actions/move.js
@@ -1,8 +1,10 @@
 import curry from 'lodash/function/curry';
 import flow from 'lodash/function/flow';
 
+import clamp from 'utils/clamp';
+
 import player from 'state/models/player';
-import coordsToId from 'state/utils/coordsToId';
+import { coordsToId } from 'state/utils/coordsToId';
 
 const xOffsets = Object.freeze({
   left:  -1,
@@ -16,16 +18,35 @@ const yOffsets = Object.freeze({
 
 export const type = 'MOVE';
 
+// TODO: `memoize`
+const terminalCell = curry((method, prop, collection) => {
+  return collection[method]((item) => item.get(prop)).get(prop);
+});
+const minCol = terminalCell('minBy', 'col');
+const maxCol = terminalCell('maxBy', 'col');
+const minRow = terminalCell('minBy', 'row');
+const maxRow = terminalCell('maxBy', 'row');
+
+const clampToWorld = curry((grounds, col, row) => {
+  return [
+    clamp(minCol(grounds), maxCol(grounds), col),
+    clamp(minRow(grounds), maxRow(grounds), row)
+  ];
+});
+
 export function reduce(state, { direction }) {
   const xOffset = xOffsets[direction] || 0;
   const yOffset = yOffsets[direction] || 0;
   const row = player.getRow(state);
   const col = player.getCol(state);
-  const newRow = row + yOffset;
-  const newCol = col + xOffset;
+  const grounds = state.get('grounds');
+  const entities = state.get('entities');
+  let [newCol, newRow] = clampToWorld(grounds, col + xOffset, row + yOffset);
   const id = coordsToId(newCol, newRow);
-  const ground = state.getIn(['grounds', id]);
-  const entity = state.getIn(['entities', id]);
+  const occupiers = [
+    grounds.get(id),
+    entities.get(id)
+  ];
   return flow(
     player.setRow(newRow),
     player.setCol(newCol),
