5a9268c1fc496b740e1176821431a0a0b846ebb4
Steven Hauser <stevenhauser@gmail.com>
2015-11-06 19:25:54 -0500

    Hook up abilities to entities

--- src/state/definitions/entities.js
+++ src/state/definitions/entities.js
@@ -1,40 +1,66 @@
+import {
+  not,
+  hasBoots,
+  hasHammer,
+  hasSilverware,
+  hasSpeedboat,
+  hasSunglasses,
+  returnTrue
+} from 'state/definitions/abilities';
+
+const canBlock = returnTrue;
+const canCollect = returnTrue;
+const canKill = returnTrue;
+const canWin = returnTrue;
+
+const blocksUnless = (hasAbility) => ({
+  canBlock: not(hasAbility),
+  canDie: hasAbility
+});
+
+const killsUnless = (hasAbility) => ({
+  canKill: not(hasAbility),
+  canDie: hasAbility
+});
+
 export const entities = {
   // Special
-  SA: { type: 'tape' },
-  SB: { type: 'door' },
+  SA: { type: 'tape', canCollect },
+  SB: { type: 'door', canWin },
   SC: { type: 'person' },
+  SD: { type: 'invisible', canBlock },
   // Powerups
-  PA: { type: 'sunglasses' },
-  PB: { type: 'silverware' },
-  PC: { type: 'speedboat' },
-  PD: { type: 'boots' },
-  PE: { type: 'hammer' },
+  PA: { type: 'sunglasses', canCollect },
+  PB: { type: 'silverware', canCollect },
+  PC: { type: 'speedboat',  canCollect },
+  PD: { type: 'boots',      canCollect },
+  PE: { type: 'hammer',     canCollect },
   // Bounds
-  BA: { type: 'treeA' },
-  BB: { type: 'treeB' },
-  BC: { type: 'building' },
-  BD: { type: 'rabbit' },
-  BE: { type: 'chicken' },
-  BF: { type: 'fishA' },
-  BG: { type: 'fishB' },
-  BH: { type: 'turtle' },
-  BI: { type: 'camel' },
-  BJ: { type: 'cloud' },
-  BK: { type: 'creepsun' },
+  BA: { type: 'treeA',    canBlock },
+  BB: { type: 'treeB',    canBlock },
+  BC: { type: 'building', canBlock },
+  BD: { type: 'rabbit',   ...blocksUnless(hasHammer) },
+  BE: { type: 'chicken',  ...blocksUnless(hasHammer) },
+  BF: { type: 'fishA',    ...blocksUnless(hasSpeedboat) },
+  BG: { type: 'fishB',    ...blocksUnless(hasSpeedboat) },
+  BH: { type: 'turtle',   ...blocksUnless(hasHammer) },
+  BI: { type: 'camel',    ...blocksUnless(hasHammer) },
+  BJ: { type: 'cloud',    canBlock },
+  BK: { type: 'creepsun', ...blocksUnless(hasHammer) },
   // Killers without items
-  DA: { type: 'sun' },
-  DB: { type: 'corn' },
-  DC: { type: 'wave' },
-  DD: { type: 'fire' },
-  DE: { type: 'snowman' },
+  DA: { type: 'sun',     canKill: not(hasSunglasses) },
+  DB: { type: 'corn',    ...blocksUnless(hasSilverware) },
+  DC: { type: 'wave',    canBlock: not(hasSpeedboat) },
+  DD: { type: 'fire',    ...killsUnless(hasBoots) },
+  DE: { type: 'snowman', ...killsUnless(hasHammer) },
   // Killers always
-  KA: { type: 'bee' },
-  KB: { type: 'gator' },
-  KC: { type: 'snake' },
-  KD: { type: 'carA' },
-  KE: { type: 'carB' },
-  KF: { type: 'taxi' },
-  KG: { type: 'firetruck' },
-  KH: { type: 'police' },
-  KI: { type: 'ambulance' },
+  KA: { type: 'bee',       canKill },
+  KB: { type: 'gator',     canKill },
+  KC: { type: 'snake',     canKill },
+  KD: { type: 'carA',      canKill },
+  KE: { type: 'carB',      canKill },
+  KF: { type: 'taxi',      canKill },
+  KG: { type: 'firetruck', canKill },
+  KH: { type: 'police',    canKill },
+  KI: { type: 'ambulance', canKill },
 };
