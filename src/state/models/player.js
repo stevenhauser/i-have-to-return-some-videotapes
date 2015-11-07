0f90396aa905460e0be41472f9a91b4032376329
Steven Hauser <stevenhauser@gmail.com>
2015-11-07 14:05:02 -0500

    Add setCoords to player API

--- src/state/models/player.js
+++ src/state/models/player.js
@@ -1,7 +1,16 @@
+import curry from 'lodash/function/curry';
+import flow from 'lodash/function/flow';
+
 import buildModel from 'state/utils/buildModel';
 
-export default buildModel('player', [
+const api = buildModel('player', [
   'row',
   'col',
   'direction'
 ]);
+
+api.setCoords = curry((col, row, state) => {
+  return flow(api.setCol(col), api.setRow(row))(state);
+});
+
+export default api;
