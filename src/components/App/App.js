f48e10d6ac0f4d64ecdd932738ec753a74d179aa
Steven Hauser <stevenhauser@gmail.com>
2015-11-08 17:25:37 -0500

    Move .navbar into App from index.html

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,9 +21,20 @@ export default createPureComponent({
       <Editor />
     );
 
+    const navbar = this.props.isEditing ? null : (
+      <nav className="navbar">
+        <iframe
+          src="https://ghbtns.com/github-btn.html?user=stevenhauser&repo=i-have-to-return-some-videotapes&type=star"
+          className="navbar__btn"
+          frameBorder="0" scrolling="0" width="50px" height="20px"
+        ></iframe>
+      </nav>
+    );
+
     return (
       <div className="app">
         {node}
+        {navbar}
       </div>
     );
   }
