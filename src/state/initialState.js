6823a479cab7675fce17f33aa8b176fa3de74121
Steven Hauser <stevenhauser@gmail.com>
2015-11-12 21:01:34 -0500

    Dispatch changeLevel when app starts
    And clean up `initialState` because the level stuff isn’t needed in it
    anymore due to the new `changeLevel` action.

--- src/state/initialState.js
+++ src/state/initialState.js
@@ -1,44 +1,13 @@
 import Immutable from 'immutable';
 
-import values from 'lodash/object/values';
-
-import {
-  parseGrounds,
-  parseEntities,
-  splitRow
-} from 'state/utils/parseLevel';
-
-import * as level1 from 'state/levels/level-01';
-
-function dataFor(key) {
-  return (
-    JSON.parse(localStorage.getItem(key)) ||
-    level1[key]
-  );
-}
-
-const entitiesData = dataFor('entities');
-const groundsData = dataFor('grounds');
-
-const entities = parseEntities(entitiesData);
-const grounds  = parseGrounds(groundsData);
-
-const numTapesTotal = values(entities)
-  .map(e => e.type)
-  .filter(t => t === 'tape')
-  .length;
-
-
 const editor = {
   activeEntity: null,
   activeGround: null,
 };
 
 export const initialState = Immutable.fromJS({
-  entities,
-  grounds,
-  numTapesTotal,
   editor: null,
+  // level: null,
   numTapes: 0,
   deaths: 0,
   time: 5 * 60,
@@ -46,8 +15,8 @@ export const initialState = Immutable.fromJS({
   health: 4,
   hasWon: false,
   player: {
-    row: groundsData.length - 1,
-    col: Math.floor(splitRow(groundsData[0]).length / 2),
+    // row: null,
+    // col: null,
     direction: 'left'
   }
 });
