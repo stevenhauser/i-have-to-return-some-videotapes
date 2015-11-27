6ede7223c001c22c9108537633a410613cd1001b
Steven Hauser <stevenhauser@gmail.com>
2015-11-27 18:47:38 -0500

    Drop AppContainer
    And render `children` in `App`

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,38 +3,21 @@ import ReactDOM from 'react-dom';
 
 import { createPureComponent } from 'utils/createPureComponent';
 
-import GameContainer from 'components/Game/GameContainer';
-import EditorContainer from 'components/Editor/EditorContainer';
-
 export default createPureComponent({
 
   displayName: 'App',
 
-  propTypes: {
-    isEditing: PropTypes.bool.isRequired
-  },
-
   render() {
-    const node = (
-      !this.props.isEditing ?
-      <GameContainer /> :
-      <EditorContainer />
-    );
-
-    const navbar = this.props.isEditing ? null : (
-      <nav className="navbar">
-        <iframe
-          src="https://ghbtns.com/github-btn.html?user=stevenhauser&repo=i-have-to-return-some-videotapes&type=star"
-          className="navbar__btn"
-          frameBorder="0" scrolling="0" width="50px" height="20px"
-        ></iframe>
-      </nav>
-    );
-
     return (
       <div className="app">
-        {node}
-        {navbar}
+        {this.props.children}
+        <nav className="navbar">
+          <iframe
+            src="https://ghbtns.com/github-btn.html?user=stevenhauser&repo=i-have-to-return-some-videotapes&type=star"
+            className="navbar__btn"
+            frameBorder="0" scrolling="0" width="50px" height="20px"
+          ></iframe>
+        </nav>
       </div>
     );
   }
