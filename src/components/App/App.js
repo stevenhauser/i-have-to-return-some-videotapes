fdbbb9ecc1913f201c7de13f49404a2a87400f3e
Steven Hauser <stevenhauser@gmail.com>
2015-11-29 12:33:11 -0500

    Render Link in App to switch b/w play and edit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,22 +1,36 @@
 import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
+import { Link } from 'react-router';
+
 import { createPureComponent } from 'utils/createPureComponent';
 
 export default createPureComponent({
 
   displayName: 'App',
 
+  renderLink() {
+    const isEditing = this.props.location.pathname === '/editor';
+    const to = isEditing ? '/' : '/editor';
+    const text = isEditing ? 'Play this level' : 'Edit this level';
+    return (<Link to={to}>{text}</Link>);
+  },
+
   render() {
     return (
       <div className="app">
         {this.props.children}
         <nav className="navbar">
-          <iframe
-            src="https://ghbtns.com/github-btn.html?user=stevenhauser&repo=i-have-to-return-some-videotapes&type=star"
-            className="navbar__btn"
-            frameBorder="0" scrolling="0" width="50px" height="20px"
-          ></iframe>
+          <div className="navbar__item">
+            {this.renderLink()}
+          </div>
+          <div className="navbar__item">
+            <iframe
+              src="https://ghbtns.com/github-btn.html?user=stevenhauser&repo=i-have-to-return-some-videotapes&type=star"
+              className="navbar__btn"
+              frameBorder="0" scrolling="0" width="50px" height="20px"
+            />
+          </div>
         </nav>
       </div>
     );
