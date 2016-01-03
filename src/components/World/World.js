f5a76cc2b6e72a31339e80b04560061e4392dd8c
Matt Campbell <matt@mattcampbell.net>
2016-01-03 15:01:35 -0500

    Use Twemoji on non-Mac platforms

--- src/components/World/World.js
+++ src/components/World/World.js
@@ -1,8 +1,11 @@
 import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
+import classNames from 'classnames';
+
 import { createPureComponent } from 'utils/createPureComponent';
 import { gridCoordsToOffsetStyle } from 'utils/gridCoordsToStyle';
+import hasEmoji from 'utils/hasEmoji';
 
 import EntitiesContainer from 'components/Entities/EntitiesContainer';
 import GroundsContainer from 'components/Grounds/GroundsContainer';
@@ -21,8 +24,9 @@ export default createPureComponent({
   render() {
     const { col, row, children } = this.props;
     const style = gridCoordsToOffsetStyle(row, col);
+    const className = classNames('world', { twemoji: !hasEmoji });
     return (
-      <div className="world" style={style}>
+      <div className={className} style={style}>
         <EntitiesContainer />
         <GroundsContainer />
         {children}
